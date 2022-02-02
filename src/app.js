78c6aca836b1fe4bcd63f645d2ec0657b9f5d0f7
Fabian Darga <kontakt@fabiandarga.de>
2022-02-02 14:01:18 +0100

    Solution

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ const app = express();
 app.use(express.json());
 
 // encrypt = verschlüsseln
+app.post('/encrypt', (req, res) => {
+    const message = req.body.message;
+    const cypher = encrypt(message);
+    res.json({
+        cypher,
+    });
+});
 
 // decrypt = entschlüsseln
+app.post('/decrypt', (req, res) => {
+    const cypher = req.body.cypher;
+    const message = decrypt(cypher);
+    res.json({
+        message,
+    });
+});
 
 export default app;
